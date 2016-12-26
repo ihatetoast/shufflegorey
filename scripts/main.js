738762d88f2a6476810570ad8e9f54bdaa42c4fe
Katy <ihatetoast@gmail.com>
2016-12-25 23:12:40 -0600

    gorey thank-you app done

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,34 @@
 $(document).ready(function(){
-	alert("butthurt.");
-	console.log("Gorey array index 0: "+goreyArray[0]);
+	//fisher-yates shuffle prototype
+	Array.prototype.shuffle = function(){
+		var index = this.length, randomIndex, temp;
+		// console.log("I'm here.");
+		while(--index > 0) {
+			//get the random number
+			randomIndex = Math.floor(Math.random() * (index+1));
+			//assign the random number to temp holder
+			temp = this[randomIndex];
+			//the swapping:
+			this[randomIndex] = this[index];
+			this[index] = temp;
+		}
+		return this;
+	}
+	var version = 0;
+	$("#helpless").click(function(){
+		var newStory = goreyArray.shuffle().join('');
+		version +=1;
+		console.log("Version is "+version+newStory);
+		
+		$("#shuffledStory").prepend(`
+			<div class="shuffledStory">
+			<h3>The Helpless Doorknob, v${version}</h3>
+			<p class="story">${newStory}</p>
+			</div>
+			`);
+	});
+	$("#clear").click(function(){
+		$("#shuffledStory").html("");
+	})
 	
 })
